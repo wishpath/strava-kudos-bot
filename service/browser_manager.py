aa25eea63dcde1724b4f74eb789333b093bea5cc
Simas <simonas.aruna@gmail.com>
2025-12-19 19:16:13 +0200

    learning_python: singleton logic

--- service/browser_manager.py
+++ service/browser_manager.py
@@ -1,29 +1,16 @@
-import logging
 from pathlib import Path
 
 from playwright.async_api import async_playwright
 from service.strava_page import StravaPage
 
-logger = logging.getLogger(__name__)
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-)
-
-STORAGE_DIR = Path(__file__).parent / 'playwright-state'
-
-
 class BrowserManager:
-    _instance = None
+    _singleton = None
 
-    def __new__(cls):
-        """Create or return the singleton instance of BrowserManager,
-        ensures there is a single browser instance in the app"""
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
+    """create sigleton object instance"""
+    def __new__(this_class):
+        this_class._singleton = super().__new__(this_class) if this_class._singleton is None else this_class._singleton
+        return this_class._singleton
+    """initial fields setter: 'self' refers to the the object returned by __new__"""
     def __init__(self) -> None:
         self.playwright = None
         self.context = None
@@ -40,13 +27,8 @@ class BrowserManager:
         if not self.playwright:
             self.playwright = await async_playwright().start()
 
-        if STORAGE_DIR.exists():
-            logger.info("Found saved authentication state")
-        else:
-            logger.info("No saved authentication state found - will save after first login")
-
         self.context = await self.playwright.chromium.launch_persistent_context(
-            user_data_dir=str(STORAGE_DIR),
+            user_data_dir=str(Path(__file__).resolve().parents[1] / "c_storage" / "playwright-state"),
             headless=False,
             channel="chrome",
             # viewport={"width": 1440, "height": 900},
